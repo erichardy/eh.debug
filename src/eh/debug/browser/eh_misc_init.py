c055d9df5fc0ab85daad479fc805a259ae707299
Eric Hardy <eric.hardy@univ-brest.fr>
2018-01-25 00:57:20 +0100

    types in navigation

--- src/eh/debug/browser/eh_misc_init.py
+++ src/eh/debug/browser/eh_misc_init.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from zope.publisher.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
 from plone import api
 import logging
 
@@ -34,5 +35,22 @@ class ehMiscInit(BrowserView):
                 except Exception:
                     logger.info('Cannot exclude from nav : ' + o.getId())
         portal.setLayout('listing_view')
-        self.request.response.redirect(portal.absolute_url())
 # http://localhost:8080/Plone/selectViewTemplate?templateId=listing_view&_authenticator=e4c1ad67a0b4df753d0209fe36c160e1ad7decaf
+
+        pprop = getToolByName(portal, 'portal_properties')
+        # ttool = getToolByName(context, 'portal_types')
+        navProps = pprop.navtree_properties
+        """
+        import pdb;pdb.set_trace()
+        (Pdb) from plone import api
+        (Pdb) dis = api.portal.get_registry_record('plone.displayed_types')
+        (Pdb) dis
+        ('Folder', 'Document')
+        (Pdb) dis = api.portal.set_registry_record('plone.displayed_types',('Folder',))
+
+        """
+        dis = api.portal.set_registry_record(
+            'plone.displayed_types',
+            ('Folder',)
+            )
+        self.request.response.redirect(portal.absolute_url())
